5d9b71f038cbe3cb64604fed5bbec9a63e8f133b
andrea <roundcrisis@gmail.com>
2015-01-21 15:38:10 +0000

    path

--- ScottyWeb/Main.hs
+++ ScottyWeb/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.Monoid (mconcat)
+import System.Directory
 import Text.Blaze.Html.Renderer.Text
 import Text.Markdown
 import Web.Scotty
@@ -11,3 +12,6 @@ main = scotty 3000 $ do
 
     let beam' = renderHtml $ markdown def beam
     html $ mconcat ["<h1>Scotty, ", beam', " me up!</h1>"]
+  get "/files" $ do
+   	  files <- liftIO $ getDirectoryContents "Datas"
+   	  html $ mconcat $ map (\ x -> "<p>"++ x ++"</p>") files
