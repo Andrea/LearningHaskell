90eaf032dde21fe155761dc25e29a4eb30179f4c
Andrea <roundcrisis@gmail.com>
2015-01-25 20:23:19 +0000

    Now files/: filename works and renders the html :D

--- ScottyWeb/Main.hs
+++ ScottyWeb/Main.hs
@@ -3,31 +3,51 @@
 import Data.Monoid (mconcat)
 import qualified Data.Text.Lazy as LT
 import Control.Monad.IO.Class (liftIO)
+import Network.HTTP.Types (status404)
 import System.Directory  
 import Text.Blaze.Html.Renderer.Text
 import Text.Markdown
-import Web.Scotty
+import Web.Scotty as S
+
 
 main = scotty 3000 $ do
-  get"/:filename" $ do
-  	--(1)I would prefer to use /files/filename, but not sure how to do that?
-  	filename <- param "filename"
-  	--(2) What is the naming convention ?, and I guess I should know this but how to turn form string to the type alias FilePath?
+  get"/files/:filename" $ do    
+    filename <- param "filename"
+    liftIO $ renderMarkdown filename
+    -- 25/01 doing the side effect first and then 
+    text <- liftIO $ renderMarkdown filename 
+    display text
+    --html $ "<p>yay! got some stuff, but not really</p>"
 
-  	markdownFile <- readFile filename
-  	-- (3) use magic to 
-  	--  		* check if the file exists , I guess
-  	--  		* get html from markdown 
-  	html $ "<p>yay! got some stuff, but not really</p>"
+--    S.status status404
   get "/files" $ do
-   	files <- liftIO $ getDirectoryContents "Datas"
-   	-- there must be a nice way to generate html and the strings ?
-   	-- How can I search for it?
-   	html $ mconcat $ map (\x -> LT.pack (mconcat ["<a>", x, "</a>"])) files
+    files <- liftIO $ getDirectoryContents "Datas"
+-- there must be a nice way to generate html and the strings ?
+-- How can I search for it?
+    html $ mconcat $ map (\x -> LT.pack (mconcat ["<a>", x, "</a>"])) files
     
   get "/:word" $ do
     beam <- param "word"
-
     let beam' = renderHtml $ markdown def beam
-    html $ mconcat ["<h1>Scotty, ", beam', " me up!</h1>"]
+    html $ mconcat ["<h1>Scotty, ",beam', " me up!</h1>"]
 
+renderMarkdown :: String -> IO (Maybe LT.Text)
+renderMarkdown filename = do
+  let f = "Datas/" ++ filename
+  exists <- doesFileExist f
+  if exists 
+    then do
+      text <- readFile f
+      return . return . renderHtml $ markdown def $ LT.pack text
+    else
+      return Nothing
+
+display :: Maybe LT.Text -> ActionM ()
+display Nothing = do
+  S.status status404
+  html $ "booo 404 for youoo"
+display (Just t) = html t
+
+--- create links to the files
+--- saves the files
+-- private function <- so that you can generate all your pages
--- ScottyWeb/readme.md
+++ ScottyWeb/readme.md
@@ -1,3 +1,7 @@
+Remember... this is a brain dump, it probably makes no sense.
+
+### Day 0
+
 Today I m going to stream trying to create a simple website with Haskell
 
 ### What we want to achieve
@@ -7,7 +11,7 @@ Today I m going to stream trying to create a simple website with Haskell
 
 ### What happened 
 
-#### Step 1
+#### Day 1
 
 * Cabal not up to date
 * run `cabal install cabal-install`
@@ -18,9 +22,29 @@ Today I m going to stream trying to create a simple website with Haskell
 
 * Just found this http://activedeveloper.info/practical-haskell-converting-markdown-to-html.html
 
-## Step 2 
+## Day 2 
 
 * Now trying to list all files in a directory 
 * The difficulty , I find is not so much on what you think you should do but understand what libs to use when, but maybe I am delusional, one example is I couldn't find what lift I should use (ie what import to use, it was ```import Control.Monad.IO.Class (liftIO)```)
+
+### Another few days
+
+Even tho I saw the ```do``` notation before, because I have to use it, I am starting to understand why this is so good, [this](http://en.wikibooks.org/wiki/Haskell/do_notation) might be useful for context
+
+```Haskell
+  do
+    text <- readFile f
+    return . return . renderHtml $ markdown def $ LT.pack text  
+```
+
+This pattern seems to be be pretty common, and that is , I guess because we are always doing stuff with the IO monad.
+
+Really liked how simple is was to compose this 404 too
+```Haskell
+display Nothing = do
+  S.status status404
+  html $ "booo 404 for youoo"
+```
+
 
 
