e1201f1169afc7b26a6cf97c87d4373457a7c820
Andrea <roundcrisis@gmail.com>
2015-01-28 17:07:49 +0000

    Successfully qualified some imports and  added Blaze using it for headers and using params in "home page"

--- ScottyWeb/Main.hs
+++ ScottyWeb/Main.hs
@@ -7,29 +7,31 @@ import Network.HTTP.Types (status404)
 import System.Directory  
 import Text.Blaze.Html.Renderer.Text
 import Text.Markdown
-import Web.Scotty as S
+import qualified Web.Scotty as S
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
 
 
-main = scotty 3000 $ do
-  get"/files/:filename" $ do    
-    filename <- param "filename"
+main = S.scotty 3000 $ do
+  S.get"/files/:filename" $ do    
+    filename <- S.param "filename"
     liftIO $ renderMarkdown filename
-    -- 25/01 doing the side effect first and then 
+    -- 25/01 doing the side effect first and then displaying
     text <- liftIO $ renderMarkdown filename 
     display text
-    --html $ "<p>yay! got some stuff, but not really</p>"
+
 
 --    S.status status404
-  get "/files" $ do
+  S.get "/files" $ do
     files <- liftIO $ getDirectoryContents "Datas"
 -- there must be a nice way to generate html and the strings ?
 -- How can I search for it?
-    html $ mconcat $ map (\x -> LT.pack (mconcat ["<a>", x, "</a>"])) files
+    S.html $ mconcat $ Prelude.map (\x -> LT.pack (mconcat ["<a>", x, "</a>"])) files
     
-  get "/:word" $ do
-    beam <- param "word"
+  S.get "/:word" $ do
+    beam <- S.param "word"
     let beam' = renderHtml $ markdown def beam
-    html $ mconcat ["<h1>Scotty, ",beam', " me up!</h1>"]
+    S.html $ mconcat ["<h1>Scotty, ",beam', " me up!</h1>"]
 
 renderMarkdown :: String -> IO (Maybe LT.Text)
 renderMarkdown filename = do
@@ -42,11 +44,11 @@ renderMarkdown filename = do
     else
       return Nothing
 
-display :: Maybe LT.Text -> ActionM ()
+display :: Maybe LT.Text -> S.ActionM ()
 display Nothing = do
   S.status status404
-  html $ "booo 404 for youoo"
-display (Just t) = html t
+  S.html $ "boo 404 for you!!"
+display (Just t) = S.html t
 
 --- create links to the files
 --- saves the files
